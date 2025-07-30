9b22491028eafb9af9fa68d8fbcba82911908c30
1011512879jero <nohabajeronimo@gmail.com>
2025-07-30 16:07:52 -0500

    template

--- backend/superparking/superparking/urls.py
+++ backend/superparking/superparking/urls.py
@@ -1,18 +1,28 @@
 from django.contrib import admin
 from django.urls import include, path 
-from apps.usuarios import *
+from apps.tiempo.views import *
+from apps.permisos.views import *
+from apps.medios_de_pago.views import *
+from apps.tiempodeparqueo.views import *
+from apps.usuarios.views import *
+from apps.vehiculo.views import *
+from apps.tipo_de_factura.views import *
+from apps.rol.views import *
+from apps.horadeparqueo.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.api.routers')),
-    path('login/', auth_views.LoginView.as_view(template_name='horadeparqueo/login.html'),
-name='login'),
-    path('registrate/', auth_views.LoginView.as_view(template_name='medios_de_pago/registrate.html'),
-name='registrate'),
-    path('pagina_inicio/', auth_views.LoginView.as_view(template_name='permisos/pagina_inicio.html'),
-name='pagina_inicio'),
-    path('rol/', auth_views.LoginView.as_view(template_name='rol/tiempo.html'),
-name='rol'),
+   
+    path('tiempo/', TiempoViewset.as_view(template_name='template/tiempo.html')),
+    path('permisos/', PermisosViewset.as_view(template_name='template/tiempo.html')),
+    path('medios_de_pago/', Medios_de_pagoViewset.as_view(template_name='template/tiempo.html')),
+    path('rol/', RolViewset.as_view(template_name='template/tiempo.html')),
+    path('tiempo_de_parqueo/', TiempodeparqueoViewset.as_view(template_name='template/tiempo.html')),
+    path('usuarios/', UsuariosViewset.as_view(template_name='template/tiempo.html')),
+    path('vehiculo/', VehiculoViewset.as_view(template_name='template/tiempo.html')),
+    path('tipo_de_factura/', Tipo_de_facturaViewset.as_view(template_name='template/tiempo.html')),
+    path('horadeparqueo/', HoradeparqueoViewset.as_view(template_name='template/tiempo.html')),
 
 
 ]
